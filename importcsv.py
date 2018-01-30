abde1a82fcf5421db06506c120d96879a37a6ef3
mccracmiler <mccracmiler@pacbell.net>
2018-01-29 20:58:22 -0800

    updated import csv and draft project writeup

--- importcsv.py
+++ importcsv.py
@@ -7,38 +7,62 @@ import pandas as pd
 import psycopg2 
 from sqlalchemy import create_engine
 
-NODES = pd.read_csv("nodes.csv")
-NODE_TAGS = pd.read_csv("nodes_tags.csv")
-WAYS = pd.read_csv("ways.csv")
-WAYS_NODES = pd.read_csv("ways_nodes.csv")
-WAYS_TAGS = pd.read_csv("ways_tags.csv")
-
-
-#con = sa.create_engine('postgresql:////localhost/openstreetmaps.db')
-#NODES = pd.read_csv('nodes.csv', chunksize=100000)
-#for node in NODES:
-#    node.to_sql(name='nodes', if_exists='append', con=con)
+NODES = pd.read_csv("nodes.csv",encoding='utf-8')
+NODE_TAGS = pd.read_csv("nodes_tags.csv",encoding='utf-8')
+WAYS = pd.read_csv("ways.csv",encoding='utf-8')
+WAYS_NODES = pd.read_csv("ways_nodes.csv",encoding='utf-8')
+WAYS_TAGS = pd.read_csv("ways_tags.csv",encoding='utf-8')
 
 
 eng = create_engine("sqlite://", encoding='utf8')
 conn = eng.connect()
-#NODES.to_sql('nodes', conn)    
-#NODE_TAGS.to_sql('node_tags',conn)      
-#WAYS.to_sql('ways',conn)
-WAYS_NODES.to_sql('ways_nodes',conn)         
-#WAYS_TAGS.to_sql('ways_tags',conn) 
+NODES.to_sql('nodes', conn)
+print "NODES ADDED to TBL"    
+NODE_TAGS.to_sql('node_tags',conn) 
+print "NODE_TAGS ADDED to TBL"        
+WAYS.to_sql('ways',conn)
+print "WAYS_NODES ADDED to TBL" 
+WAYS_NODES.to_sql('ways_nodes',conn) 
+print "WAYS_NODES ADDED to TBL"         
+WAYS_TAGS.to_sql('ways_tags',conn) 
+print "WAYS_TAGS ADDED to TBL" 
 print "Process complete"
-#http://odo.pydata.org/en/latest/perf.html
+
+#sql queries
+distinct_users = pd.read_sql("SELECT COUNT(DISTINCT NODES.uid) as distinctusers \
+        FROM NODES JOIN WAYS on WAYS.uid = NODES.uid",eng)
+print "Distinct Users:", distinct_users  
 
 
-#sql
-#pd.read_sql ("SELECT tags.value, COUNT(*) as count 
-#             FROM (SELECT * FROM nodes_tags 
-#                   ON ALL 
-#                   SELECT * FROM ways_tags) tags
-#                   WHERE tags.key='postcode'
-#                   GROUP BY tags.value
-#                   ORDER BY count DESC", conn)
+amenities = pd.read_sql("SELECT value, COUNT(*) as num FROM NODE_TAGS \
+        WHERE key='amenity' GROUP BY value ORDER BY num DESC \
+        LIMIT 10",eng);
+print amenities
 
+distinct_users = pd.read_sql("SELECT COUNT(DISTINCT nodes.uid) as distinctusers \
+        FROM nodes JOIN ways on ways.uid = nodes.uid",eng)
+print "Distinct Users:", distinct_users  
 
+#Most active users
+most_active_nodes_users = pd.read_sql("SELECT user, COUNT(*) as num FROM nodes \
+        GROUP BY user ORDER by num DESC Limit 3",eng);
+print "Most active nodes contributors:", most_active_nodes_users
 
+most_active_way_users = pd.read_sql("SELECT user, COUNT(*) as num FROM ways \
+        GROUP BY user ORDER by num DESC Limit 3",eng);
+print "Most active ways contributors:", most_active_way_users
+
+getid = pd.read_sql("SELECT id FROM node_tags \
+        WHERE value = 'Notre Dame de Namur University'",eng);
+print "Get id:", getid
+
+getdata = pd.read_sql("SELECT key, value FROM node_tags \
+        WHERE id = '358763902'",eng);
+print "Data:", getdata
+
+getdata2 = pd.read_sql("SELECT key, value FROM node_tags\
+        (SELECT id AS getid FROM node_tags WHERE value = 'Notre Dame de Namur University')\
+          AS subquery WHERE id = getid",eng);
+print "Data:", getdata
+
+ 
